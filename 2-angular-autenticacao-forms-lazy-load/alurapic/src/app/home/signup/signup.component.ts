b67dba37b93cc19175caab8e4b0ef6021ad10139
Bruno C A Gaudino <brunocesaragweb@gmail.com>
2019-04-10 12:31:28 -0300

    Finalização do modulo2 curso, rotas filhas, buid do projeto, lazy loading, code splitting, interceptadores e envio de token

--- 2-angular-autenticacao-forms-lazy-load/alurapic/src/app/home/signup/signup.component.ts
+++ 2-angular-autenticacao-forms-lazy-load/alurapic/src/app/home/signup/signup.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/components/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 import { SignUpService } from './signup.service';
 import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
-    templateUrl: './signup.component.html'
+    templateUrl: './signup.component.html',
+    providers: [ UserNotTakenValidatorService ]
 })
 export class SignUpComponent implements OnInit{
     
     signupForm: FormGroup;
+    @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
     
     constructor(
         private formBuilder: FormBuilder,
         private userNotTakenValidatorService: UserNotTakenValidatorService,
         private signUpService: SignUpService,
-        private router: Router
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
     ){}
 
     ngOnInit(): void {
@@ -51,6 +55,9 @@ export class SignUpComponent implements OnInit{
                 ]
             ]
         });
+
+        this.platformDetectorService.isPlatformBrowser() && 
+        this.emailInput.nativeElement.focus();
     }
 
     signup() {
