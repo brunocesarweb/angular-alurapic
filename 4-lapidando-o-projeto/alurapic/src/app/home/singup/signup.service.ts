dff219315dc3449c97e712a0bb3be5b47f881655
GabrielDev <gabriel.dos.santos.lucena@gmail.com>
2020-02-20 11:48:52 -0300

    Angular atualizado para ultima versao

--- 4-lapidando-o-projeto/alurapic/src/app/home/singup/signup.service.ts
+++ 4-lapidando-o-projeto/alurapic/src/app/home/singup/signup.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NewUser } from './new-user';
-import { provideForRootGuard } from '@angular/router/src/router_module';
-
 import { environment } from '../../../environments/environment';
 
 const API = environment.ApiUrl;
